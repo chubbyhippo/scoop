93319d2712caa59ee7f1d3c562a1a611f41f9bc5
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-02 10:41:31 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,9 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-scoop bucket add extras
 
+scoop bucket add main
+scoop install main/git
+
+scoop bucket add extras
 scoop install extras/autohotkey
