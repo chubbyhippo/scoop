d812a6648f4b6c6c0970e1ce22e6fc0062e05d94
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-18 12:47:15 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -15,6 +15,7 @@ scoop install main/go
 scoop install main/gradle
 scoop install main/jbang
 scoop install main/jq
+scoop install main/leiningen
 scoop install main/maven
 scoop install main/minikube
 scoop install main/msys2
