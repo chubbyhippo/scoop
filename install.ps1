95d6376f60e96f08c35474d8f8c5e532700dae79
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-09 20:32:19 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -37,6 +37,7 @@ scoop bucket add extras
 scoop install extras/autohotkey
 scoop install extras/chromium
 scoop install extras/dbeaver
+scoop install extras/emacs
 scoop install extras/firefox
 scoop install extras/gimp
 scoop install extras/hoppscotch
