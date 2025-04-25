ce095812ad8ccf251ceb989b134f198ca25c53df
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-25 08:38:35 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -22,7 +22,6 @@ scoop install main/mise
 scoop install main/msys2
 scoop install main/neovim
 scoop install main/pandoc
-scoop install main/podman
 scoop install main/racket
 scoop install main/ripgrep
 scoop install main/rustup
