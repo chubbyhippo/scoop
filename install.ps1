d0f266325418d2eaa19cb62e28d2f619e7512ba3
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-06-02 15:05:21 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -64,8 +64,8 @@ scoop install java/visualvm
 scoop bucket add nonportable
 Start-Process powershell -ArgumentList "-Command `"scoop install nonportable/virtualbox-with-extension-pack-np`"" -Verb RunAs
 
-scoop bucket add sysinternals
 scoop bucket add nerd-fonts
 scoop install nerd-fonts/JetBrains-Mono
 
+scoop bucket add sysinternals
 scoop install sysinternals/zoomit
