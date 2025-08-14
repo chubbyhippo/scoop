c710c6f7a66dfc727664051609f2297b99754b11
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-14 19:00:45 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -42,6 +42,7 @@ scoop install main/vimtutor
 scoop install main/wget
 scoop install main/winfetch
 scoop install main/yt-dlp
+scoop install main/zstd
 
 scoop bucket add extras
 scoop install extras/autohotkey
