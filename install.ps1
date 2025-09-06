1520a38e433077b55b1b5c22ccb547d54147eaba
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-09-06 14:32:10 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -16,6 +16,7 @@ scoop install main/git
 git config --global credential.helper manager
 reg import "$Home\scoop\apps\git\current\install-context.reg"
 scoop install main/fastfetch
+scoop install main/gnutls
 scoop install main/gradle
 scoop install main/grype
 scoop install main/imagemagick
