b61b9127aefb529d9e940dbfcd28f7a8bc86322f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-01-30 14:28:32 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -71,7 +71,6 @@ scoop install extras/dbeaver
 scoop install extras/emacs
 scoop install extras/firefox
 scoop install extras/gimp
-scoop install extras/hoppscotch
 scoop install extras/inkscape
 scoop install extras/jetbrains-toolbox
 scoop install extras/krita
