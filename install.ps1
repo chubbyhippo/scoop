af17cb09e54f5b777a67f557ae07c8d7be1c8224
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-02-24 17:41:07 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -40,6 +40,7 @@ scoop install main/pandoc
 scoop install main/pnpm
 scoop install main/python
 reg import "$Home\scoop\apps\python\current\install-pep-514.reg"
+scoop install main/quarkus-cli
 scoop install main/racket
 scoop install main/ripgrep
 scoop install main/ruby
