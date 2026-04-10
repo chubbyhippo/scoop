cb8365afd3884e57d9fe3d5a7176d661483d1bda
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-10 20:14:07 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -13,7 +13,6 @@ scoop install main/fd
 scoop install main/fzf
 scoop install main/gcc
 scoop install main/gdb
-scoop install main/ghostscript
 scoop install main/git
 git config --global credential.helper manager
 reg import "$Home\scoop\apps\git\current\install-context.reg"
