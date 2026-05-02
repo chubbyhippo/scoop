1bb8eb13674d29a399acdbce28b592202d77bbec
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-02 22:02:03 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -65,6 +65,7 @@ scoop install extras/krita
 scoop install extras/lazygit
 scoop install extras/obs-studio
 scoop install extras/qbittorrent
+scoop install extras/rancher-desktop
 scoop install extras/rufus
 scoop install extras/soapui
 scoop install extras/springboot
