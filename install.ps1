6c8c1851f3d733724203f536367b191cc844ecb4
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-26 08:02:53 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -35,6 +35,7 @@ scoop install main/neovim
 scoop install main/ninja
 scoop install main/pandoc
 scoop install main/php
+scoop install main/racket
 scoop install main/ripgrep
 scoop install main/roswell
 scoop install main/shellcheck
